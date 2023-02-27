6ea67bc1ba193f3752779950e63fc186cfd2e826
ɴᴧʀᴜᴛᴏ ᴜᴢᴜᴍᴀᴋɪ <111524889+phoenix-monarch@users.noreply.github.com>
2023-02-27 22:37:16 +0530

    Add files via upload

---
+++ next-i18next.config.js
@@ -0,0 +1,21 @@
+const HttpBackend = require("i18next-http-backend/cjs");
+const ChainedBackend = require("i18next-chained-backend").default;
+const LocalStorageBackend = require("i18next-localstorage-backend").default;
+
+const isDev = process.env.NODE_ENV === "development";
+
+module.exports = {
+  backend: {
+    backendOptions: [{ expirationTime: isDev ? 60 : 7 * 24 * 60 * 60 * 1000 }], //  7 days
+    backends:
+      typeof window !== "undefined" ? [LocalStorageBackend, HttpBackend] : [],
+  },
+  i18n: {
+    locales: ["vi", "en", "ru", "es"],
+    defaultLocale: "en",
+    reloadOnPrerender: isDev,
+    load: "currentOnly",
+  },
+  serializeConfig: false,
+  use: typeof window !== "undefined" ? [ChainedBackend] : [],
+};
